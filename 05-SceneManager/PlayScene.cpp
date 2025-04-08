9f05ea83e4ef22d6bf7b709b52f26699768ec2c6
cblnam412 <23520979@gm.uit.edu.vn>
2025-04-08 13:56:00 +0700

    Thêm đối tượng Koopas

--- 05-SceneManager/PlayScene.cpp
+++ 05-SceneManager/PlayScene.cpp
@@ -8,6 +8,7 @@
 #include "Sprites.h"
 #include "Portal.h"
 #include "Coin.h"
+#include "Koopas.h"
 #include "Platform.h"
 
 #include "SampleKeyEventHandler.h"
@@ -120,7 +121,7 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	case OBJECT_TYPE_BRICK: obj = new CBrick(x,y); break;
 	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
 	case OBJECT_TYPE_MUSHROOM: obj = new CMushroom(x, y); break;
-
+	case OBJECT_TYPE_KOOPAS: obj = new CKoopas(x, y); break;
 	case OBJECT_TYPE_PLATFORM:
 	{
 
